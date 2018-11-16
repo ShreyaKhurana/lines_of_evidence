b2548f09abc115a6db5fde7dee3a24e95b0a5255
shreyak4 <shreyak4@illinois.edu>
2018-11-16 04:46:59 -0600

    dist,qnorm,changed max

--- lines_of_evidence_mcmc.R
+++ lines_of_evidence_mcmc.R
@@ -71,9 +71,9 @@ tau <- 25
 a <- 1
 b <- 5
 w <- 4
-A <- 0.01
-B <- 0.05
-phi_sd <- 2
+A <- 0.2
+B <- 0.3
+phi_sd <- 0.2
 
 init0 <- rnorm(3, 0, sd=sqrt(tau))
 init1 <- rnorm(3, 1, sd=sqrt(tau))
@@ -116,114 +116,129 @@ mu[1] <- rnorm(1, 0, w)
 x <- matrix(0, nrow = n, ncol = tt)
 
 phi[1] <-  runif(1, A, B)
-
+aa = 0.826
 # Some helper functions
 x.dist <- as.matrix(dist(coords))
-x.dist <- apply(x.dist, MARGIN = 2, FUN = function(X) (X - min(X))/diff(range(X)))
-V <- function(phi) {return(exp(-x.dist/phi))}
-Sigma.inv <- function(sigma.2, V) {return(ginv(sigma.2*V))}
+minx = min(x.dist)
+rangex = diff(range(x.dist))
+x.dist <- apply(x.dist, MARGIN = 1, FUN = function(X) (X - minx)/rangex)
+V <- function(phi) {return(exp(-x.dist/phi) + aa*diag(1065))}
+Sigma.inv <- function(sigma.2, V) {return(sigma.2*solve(V))}
 
 phi_target <- function(phi0, sigma2, x1, mu0){
   if (phi0 >= A && phi0 <= B){
-    return((det(V(phi0))^-0.5) * exp(1/(2*sigma2) * (t(x1 - mu0) %*% ginv(V(phi0)) %*% (x1 - mu0))))
+    return((det(V(phi0))^-0.5) * exp(1/(2*sigma2) * (t(x1 - mu0) %*% solve(V(phi0)) %*% (x1 - mu0))))
   }
   return(0)
 }
 
+makeSymm <- function(m) {
+  m[upper.tri(m)] <- t(m)[upper.tri(m)]
+  return(m)
+}
 # Metropolis hastings algorithm
-MH <- function(old, sigma2, x1, mu0, sd) {
-  proposed <-  old + rnorm(1, mean=0, sd=sd)
-  ratio <- min(1, phi_target(proposed, sigma2, x1, mu0)/phi_target(old, sigma2, x1, mu0))
-  # print(ratio)
-  if (runif(1) <= ratio) { return(proposed) }
+# MH <- function(old, sigma2, x1, mu0, sd) {
+#   proposed <-  old + rnorm(1, mean=0, sd=sd)
+#   ratio <- min(1, phi_target(proposed, sigma2, x1, mu0)/phi_target(old, sigma2, x1, mu0))
+#   # print(ratio)
+#   if (runif(1) <= ratio) { return(proposed) }
+#   return(old)
+# }
+
+MH <- function(old, sigma2, x1, mu0) {
+  proposed <-  old + rnorm(1, mean=0, sd=phi_sd)
+  print(proposed)
+  a = -1/(2*sigma2) * (t(x1 - mu0) %*% solve(V(proposed), (x1 - mu0)))
+  b = -1/(2*sigma2) * (t(x1 - mu0) %*% solve(V(old), x1 - mu0))
+  ratio <- min(1, sqrt(det(V(old)) / det(V(proposed))) * exp(a-b))
+  if(runif(1) <= ratio) { return(proposed) }
   return(old)
 }
 
 for (i in 2:tt) {
-  # Try for one timestep
-  # i=2
+# Try for one timestep
+# i=2
   mm <- beta1[i-1]/sigma2.m[i-1] * t(M) %*% (x.m - beta0[i-1])
   gg <- alpha1[i-1]/sigma2.g[i-1] * t(G) %*% (x.g - alpha0[i-1])
   pp <- gamma1[i-1]/sigma2.p[i-1] * t(P) %*% (x.p - gamma0[i-1])
   sinv <- Sigma.inv(sigma.2[i-1], V(phi[i-1]))
-  
+
   mu.tilda <- mu[i-1]*rowSums(sinv) + mm + gg + pp
-  
+
   mm <- beta1[i-1]/sigma2.m[i-1] * (t(M) %*% M)
   gg <- alpha1[i-1]/sigma2.g[i-1] * (t(G) %*% G)
   pp <- gamma1[i-1]/sigma2.p[i-1] * (t(P) %*% P)
-  
-  Sigma.tilda <- ginv(sinv + mm + gg + pp)
-  
+
+  Sigma.tilda <- makeSymm(solve(sinv + mm + gg + pp))
+
   # Sample x
   x[,i] <- mvrnorm(mu = Sigma.tilda %*% mu.tilda, Sigma = Sigma.tilda)
-  
+
   # Sample mu
   k <- n/sigma.2[i-1] + 1/w
   mu[i,] <- rnorm(1, (rep(1,n) %*% sinv %*% x[,i]) / k, sqrt(1/k))
-  
+
   # Sample sigma.2
-  bb <- 0.5*t(x[,i] - mu[i,]) %*% ginv(V(phi[i-1])) %*% (x[,i] - mu[i,]) + b
+  bb <- 0.5*t(x[,i] - mu[i,]) %*% solve(V(phi[i-1])) %*% (x[,i] - mu[i,]) + b
   sigma.2[i,] <- 1/rgamma(1, shape = n/2 + a, scale = bb)
-  
+
   # Sample sigma2.m, sigma2.g, sigma2.p
   aa <- n.m/2 + a
   bb <- b + 0.5*sum((x.m - beta0[i-1] - beta1[i-1]*x[1:n.m,i])^2)
   sigma2.m[i] <- 1/rgamma(1,shape=aa,scale=bb)
-  
+
   aa <- n.g/2 + a
   bb <- b + 0.5*sum((x.g - alpha0[i-1] - alpha1[i-1]*x[(n.m+1):(n.m+n.g),i])^2)
   sigma2.g[i] <- 1/rgamma(1,shape=aa,scale=bb)
-  
+
   aa <- n.p/2 + a
   bb <- b + 0.5*sum((x.p - gamma0[i-1] - gamma1[i-1]*x[(n.m+n.g+1):n,i])^2)
   sigma2.p[i] <- 1/rgamma(1,shape=aa,scale=bb)
-  
+
   # Sample beta1
   c.k <- 1/n.m * (t(x.m) %*% x[1:n.m,i])
   s.k <- 1/n.m * (t(x[,i]) %*% t(M) %*% M %*% x[,i])
   eta.k <- n.m * s.k / sigma2.m[i] + 1/tau
-  
+
   mn <- n.m/sigma2.m[i] * (c.k - beta0[i-1]*mean(x[1:n.m,i])) + 1/tau
-  
+
   beta1[i] <- rnorm(1, mean = mn / eta.k, sd = sqrt(1/eta.k))
-  
+
   # Sample alpha1
-  
+
   c.k <- 1/n.g * (t(x.g) %*% x[(n.m+1):(n.m + n.g),i])
   s.k <- 1/n.g * (t(x[,i]) %*% t(G) %*% G %*% x[,i])
   eta.k <- n.g * s.k / sigma2.g[i] + 1/tau
-  
+
   mn <- n.g/sigma2.g[i] * (c.k - alpha0[i-1]*mean(x[(n.m+1):(n.m + n.g),i])) + 1/tau
-  
+
   alpha1[i] <- rnorm(1, mean = mn / eta.k, sd = sqrt(1/eta.k))
-  
+
   # Sample gamma1
-  
+
   c.k <- 1/n.p * (t(x.p) %*% x[(n.m+n.g+1):n,i])
   s.k <- 1/n.p * (t(x[,i]) %*% t(P) %*% P %*% x[,i])
   eta.k <- n.p * s.k / sigma2.p[i] + 1/tau
-  
+
   mn <- n.p/sigma2.p[i] * (c.k - gamma0[i-1]*mean(x[(n.m+n.g+1):n,i])) + 1/tau
-  
+
   gamma1[i] <- rnorm(1, mean = mn / eta.k, sd = sqrt(1/eta.k))
-  
+
   # Sample beta0
   rho.k <- n.m/sigma2.m[i] + 1/tau
   mn <- n.m/sigma2.m[i]*(mean(x.m) - beta1[i]*mean(x[1:n.m,i]))
   beta0[i] <- rnorm(1, mean = mn/rho.k, sd = 1/sqrt(rho.k))
-  
+
   # Sample alpha0
   rho.k <- n.g/sigma2.g[i] + 1/tau
   mn <- n.g/sigma2.g[i]*(mean(x.g) - alpha1[i]*mean(x[(n.m+1):(n.m + n.g),i]))
   alpha0[i] <- rnorm(1, mean = mn/rho.k, sd = 1/sqrt(rho.k))
-  
+
   # Sample gamma0
   rho.k <- n.p/sigma2.p[i] + 1/tau
   mn <- n.p/sigma2.p[i]*(mean(x.p) - gamma1[i]*mean(x[(n.m+n.g+1):n,i]))
   gamma0[i] <- rnorm(1, mean = mn/rho.k, sd = 1/sqrt(rho.k))
-  
+
   # Sample phi by Metropolis Hastings
   phi[i] <- MH(phi[i-1], sigma.2[i], x[,i], mu[i], phi_sd)
 }
-
